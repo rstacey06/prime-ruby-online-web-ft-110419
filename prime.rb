14ad91c2ed11e80f9575a4370076bf86f1384825
Burbage Stacey <rstacey06@gmail.com>
2019-11-08 14:08:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(number)
-  if number < 0 or number == 0 or num == 1
+  if number == 0 or number < 0 or num == 1
     return false
   else
-    (2..num-1).to_a.all? do |possible_factor|
+    (2..number -1).to_a.all? do |x|
       num % possible_factor != 0
     end
   end
